82190c48e3b470b5eed191d7d4aa8516691aeaff
Jimmy Lu <gongchuo.lu@gmail.com>
2015-12-19 18:19:21 -0500

    Update dependencies & add tests

--- nreplds/project.clj
+++ nreplds/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/monsanto/nreplds"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.nrepl "0.2.3"]
-                 [org.newsclub/junixsocket "1.3"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]
+                 [com.kohlschutter.junixsocket/junixsocket-native-common "2.0.4"]])
